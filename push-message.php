3755a723424b76fa5c08ebba2394eeee96aae617
nuttawoot <nuttawoot.s@app-alive.com>
2016-11-19 12:07:43 +0700

    add push message

---
+++ push-message.php
@@ -0,0 +1,28 @@
+<?php
+$access_token = 'VSGQkinYYpJsnFpuYZaZMjukt3LCdCiFO9FXrg5dt52MxWKS2+DOOK95DAe79kDCxqPVkKr0ZT9Nqm+91YDqZwRhnrXT8U1SPeeduANJuP+YhWZe8egKGu/yFjs6kwFZN56IjNAos/rXKOxcRls1bAdB04t89/1O/w1cDnyilFU=';
+
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/push';
+$data = [
+	'to' => 'kurozakizz',
+	'messages' => [
+		[0] => [
+			'type' => 'text',
+			'message' = 'สวัสดีฉนคือนายเมจิน'
+		]
+	],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result . "\r\n";
+echo "OK";
