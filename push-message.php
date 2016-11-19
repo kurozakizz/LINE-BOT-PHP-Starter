1d7e75f1bc3fea56923b4f075d3e9c4e5137f287
nuttawoot <nuttawoot.s@app-alive.com>
2016-11-19 22:37:52 +0700

    change push logic

--- push-message.php
+++ push-message.php
@@ -1,16 +1,19 @@
 <?php
 $access_token = 'VSGQkinYYpJsnFpuYZaZMjukt3LCdCiFO9FXrg5dt52MxWKS2+DOOK95DAe79kDCxqPVkKr0ZT9Nqm+91YDqZwRhnrXT8U1SPeeduANJuP+YhWZe8egKGu/yFjs6kwFZN56IjNAos/rXKOxcRls1bAdB04t89/1O/w1cDnyilFU=';
 
+$to = $_GET['to']; //'U2bfe4a2f9bbfdc8f03ff26d96fbf7675'
+
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/push';
+
+$messages = [
+	'type' => 'text',
+	'text' => 'Adidas All Bird ของคุณได้ถูกรับหิ้วแล้วกรุณาเข้าไปชำระเงิน',
+];
+
 $data = [
-	'to' => 'U2bfe4a2f9bbfdc8f03ff26d96fbf7675',
-	'messages' => [
-		[0] => [
-			'type' => 'text',
-			'message' => 'สวัสดีฉนคือนายเมจิน'
-		]
-	],
+	'to' => $to,
+	'messages' => [$messages]
 ];
 $post = json_encode($data);
 
