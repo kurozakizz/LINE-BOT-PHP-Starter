89b5ab44b86a699851f2999730ca7775b9cd77a6
nuttawoot <nuttawoot.s@app-alive.com>
2016-11-19 22:56:45 +0700

    ..

--- push-message.php
+++ push-message.php
@@ -2,7 +2,7 @@
 $access_token = 'VSGQkinYYpJsnFpuYZaZMjukt3LCdCiFO9FXrg5dt52MxWKS2+DOOK95DAe79kDCxqPVkKr0ZT9Nqm+91YDqZwRhnrXT8U1SPeeduANJuP+YhWZe8egKGu/yFjs6kwFZN56IjNAos/rXKOxcRls1bAdB04t89/1O/w1cDnyilFU=';
 
 $to = $_GET['to']; //'U2bfe4a2f9bbfdc8f03ff26d96fbf7675'
-$msg = $_GET['mgs'];
+$msg = $_GET['msg'];
 
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/push';
