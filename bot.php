261ce5c4d43acb2ec2c9d123b847fd3f89516f97
nuttawoot <nuttawoot.s@app-alive.com>
2016-11-19 21:55:07 +0700

    add content to response

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text . $content
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
